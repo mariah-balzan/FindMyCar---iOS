a63751fea62000ac5038ea6a7e20b60f0103a2b2
Nicole-bezzina <82512545+Nicole-bezzina@users.noreply.github.com>
2023-01-15 13:48:32 +0100

    Bottom nav commit

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,49 +1,93 @@
-import React from "react";
-import {View, Text, Button, StyleSheet, TouchableOpacity} from "react-native";
-import { auth } from "../firebase";
+import { TabActions, useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { auth } from '../firebase'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from './ProfileScreen';
+import SettingsScreen from './SettingsScreen';
 
+const Tab = createBottomTabNavigator()
 
-
-export default function HomeScreen ({navigation}){
-
-    const handleSignOut = () => {
-        auth
-          .signOut()
-          .then(() => {
-            navigation.navigate("LoginRegister")
-          })
-          .catch(error => alert(error.message))
-      }
-    return(
-        <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-    )
+const Home = () => {
+  return (
+      <Tab.Navigator>
+        <Tab.Screen options = {{headerShown:false}} name="Profile" component={ProfileStack} />
+        <Tab.Screen options = {{headerShown:false}} name="Home" component={HomeStack} />
+        <Tab.Screen options = {{headerShown:false}} name="Settings" component={SettingsStack} />
+      </Tab.Navigator>
+  );
 }
 
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeFunction} />
+    </Stack.Navigator>
+  );
+}
+
+const HomeFunction = () => {
+  const navigation = useNavigation()
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+  return (
+    <View style={styles.container}>
+          <Text>Email: {auth.currentUser?.email}</Text>      
+          <TouchableOpacity
+            onPress={handleSignOut}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Sign out</Text>
+          </TouchableOpacity>
+    </View>
+  );
+}
+
+const ProfileStack = () => {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+  );
+}
+
+const SettingsStack = () => {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+  );
+}
+
+export default Home
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-       button: {
-        backgroundColor: '#0782F9',
-        width: '60%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 40,
-      },
-      buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-      },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+   button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+})
