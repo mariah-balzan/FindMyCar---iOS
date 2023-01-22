8150608d9a6a35f721e7bdc9021fd3a18935022e
Mariah Balzan <mariah.balzan.20@um.edu.mt>
2023-01-22 22:01:34 +0100

    Increased font size for onboarding subtitle

--- Screens/OnBoarding.jsx
+++ Screens/OnBoarding.jsx
@@ -44,13 +44,13 @@ if(!fontsLoaded){
                 backgroundColor: "#F4F7F8",
                 image: <Image style={{width:320, height:320}}source= {require("../assets/screen1.png")}/>,
                 title: <Text style= {{color:'#219EBC', fontSize:'43', fontFamily:'Comfortaa'}}>Find my car</Text>,
-                subtitle: <Text style= {{color:'#219EBC', fontSize:'16', fontFamily:'Comfortaa', paddingTop:'10%'}}>Park your car wherever, whenever.</Text>,
+                subtitle: <Text style= {{color:'#219EBC', fontSize:'19', fontFamily:'Comfortaa', paddingTop:'10%'}}>Park your car wherever, whenever.</Text>,
                 },
                 {
                     backgroundColor: '#8ECAE6',
                     image: <Image style={{resizeMode: "stretch",width: width * 1.1, height:height/3}}source= {require("../assets/screen3.png")} />,
                     title: <Text style= {{color:'#F4F7F8', fontSize:'35', fontFamily:'Comfortaa'}}>We got you covered</Text>,
-                    subtitle:  <Text style= {{color:'#F4F7F8', fontSize:'15', fontFamily:'Comfortaa', paddingTop:'10%'}}>Easily locate and get to your car in seconds</Text>,
+                    subtitle:  <Text style= {{color:'#F4F7F8', fontSize:'19', fontFamily:'Comfortaa', paddingTop:'10%',textAlign:'center' }}>Easily locate and get to your car in seconds.</Text>,
                 },
             ]}
             />
